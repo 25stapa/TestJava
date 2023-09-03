0d1e07a6d8f9bc3ab6c98bcbff264ad778884c75
25stapa <122109573+25stapa@users.noreply.github.com>
2023-09-03 10:33:17 +0300

    Initial commit

---
+++ src/ToyStore.java
@@ -0,0 +1,10 @@
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
